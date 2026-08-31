b1fd552adf0cc61a29e6d2708d80c7b613488141
Aryan Singh <arayns191@gmail.com>
2026-08-31 01:20:16 -0700

    Prepare backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -105,8 +105,8 @@ app.get("/api/admin/attendance", (req, res) => {
   });
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
